31607bf2b1bc6c679c39e4df23e915f8ce51cbee
Aayush Mishra <manofganges@gmail.com>
2025-06-25 22:23:00 +0530

    Update Highlights.jsx

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -14,15 +14,15 @@ const Highlights = () => {
     <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
       <div className="screen-max-width">
         <div className="mb-12 w-full md:flex items-end justify-between">
-          <h1 id="title" className="section-heading">Get the highlights.</h1>
+          <h1 id="title" className="section-heading">Get the Highlights.</h1>
 
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
-              Watch the film
+              watch the film
               <img src={watchImg} alt="watch" className="ml-2" />
             </p>
             <p className="link">
-              Watch the event
+              watch the event
               <img src={rightImg} alt="right" className="ml-2" />
             </p>
           </div>
